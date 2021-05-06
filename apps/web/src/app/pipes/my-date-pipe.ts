607d6915254e31719db624cf5e5a6577c7f1555e
Noah <65568595+Noahkosy@users.noreply.github.com>
2021-05-06 16:54:14 -0400

    Added directives, resolves, pipe, and validator
    Yeah so the file structure is a mess haha. But these are my experiments and tinkering around with these new concepts.

---
+++ apps/web/src/app/pipes/my-date-pipe.ts
@@ -0,0 +1,21 @@
+import { Injectable, Pipe } from "@angular/core";
+
+@Pipe({
+    name: 'myDatePipe'
+})
+
+@Injectable()
+export class myDatePipe {
+
+    skyrimMonths: string[] = ["Morning Star", "Sun's Dawn", "First Seed", "Rain's Hand", "Second Seed", "Mid Year", 
+    "Sun's Height", "Last Seed", "Hearthfire", "Frost Fall", "Sun's Dusk", "Evening Star"]
+
+    transform(str) {
+        let year = str.slice(0,4);
+        let day = str.slice(9,10);
+        let month = str.slice(6,7);
+        let conMonth = this.skyrimMonths[month]
+        let result = conMonth + ", " + day + ", " + year
+        return result;
+    }
+}
