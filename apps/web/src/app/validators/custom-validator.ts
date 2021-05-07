57c3e5e2142ede018b36ada548a8e7efa6b5465e
Noah <65568595+Noahkosy@users.noreply.github.com>
2021-05-07 13:25:01 -0400

    Delete apps/web/src/app/validators directory

--- apps/web/src/app/validators/custom-validator.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractControl } from "@angular/forms";
-
-export function PrefixValidation (control: AbstractControl) {
-    if (control.value.includes("arc-")){
-        return { reservedPrefix: true };
-    }
-}
