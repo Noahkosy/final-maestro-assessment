901ac82bb6fcc52843f4dba4071d2a9b02c101eb
Noah <noah@meetmaestro.com>
2021-05-11 12:02:44 -0400

    feat(tasks/validation): Fixed variable syntax
    just committing small things to get used to it.

--- apps/web/src/app/tasks/task-add/future-date.validator.ts
+++ apps/web/src/app/tasks/task-add/future-date.validator.ts
@@ -1,9 +1,9 @@
 import { AbstractControl } from "@angular/forms";
 
 export function FutureDateValidation (ctrl: AbstractControl) {
-    let str = ctrl.value;
-    let now = parseInt(new Date().toISOString().slice(0,10).split('-').join(''));
-    let date = parseInt(str.replaceAll("-", ''));
+    const str = ctrl.value;
+    const now = parseInt(new Date().toISOString().slice(0,10).split('-').join(''));
+    const date = parseInt(str.replaceAll("-", ''));
     if (date < now) {
         return { invalidDate: true };
     } else {
