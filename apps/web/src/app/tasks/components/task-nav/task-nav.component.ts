62b744605dd3eb45f4ede14c304e4a97781a76e8
Noah <noah@meetmaestro.com>
2021-05-17 16:40:13 -0400

    Feat(tasks/nav): Navbar is now its own component
    Navbar is now styled appropriately and uses ng-content to bring in buttons and titles.

---
+++ apps/web/src/app/tasks/components/task-nav/task-nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'nxlp-task-nav',
+    templateUrl: './task-nav.component.html',
+    styleUrls: ['./task-nav.component.scss']
+})
+
+export class TaskNavComponent implements OnInit {
+
+    constructor(){}
+
+    ngOnInit(): void {
+        
+    }
+}
