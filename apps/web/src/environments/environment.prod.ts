c1f239a61887d06017b20be6b5d9f02e68a792c2
Noah <noah@meetmaestro.com>
2021-06-01 14:07:02 -0400

    fix(app): New database
    I'm lazy and didn't want to configure new firebase rules so I just created a new one.

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyChG8vYV3zMfwmgnRmSptXXNS0zY31Bojk",
-    authDomain: "maestro-demo-ed009.firebaseapp.com",
-    projectId: "maestro-demo-ed009",
-    storageBucket: "maestro-demo-ed009.appspot.com",
-    messagingSenderId: "1027294287588",
-    appId: "1:1027294287588:web:ca8ca8dbeb1e4a8f0ebef1"
+    apiKey: "AIzaSyCBoQOsysYRbfgFU7t55y8LTEAg8mB_cfo",
+    authDomain: "maestro-demo-b1176.firebaseapp.com",
+    projectId: "maestro-demo-b1176",
+    storageBucket: "maestro-demo-b1176.appspot.com",
+    messagingSenderId: "132352731724",
+    appId: "1:132352731724:web:a0067fe00c7de5696822e9"
   }
 };
