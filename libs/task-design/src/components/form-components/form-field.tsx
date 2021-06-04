d238a2bb880ab73a9aad55b4a278aba49dccacab
Noah <noah@meetmaestro.com>
2021-06-04 10:34:17 -0400

    Feat(tasks/form): Form validation updates.
    Forms now check for validation before the form is submitted and will show appropriately. 
    
    Also custom validators and pipes now check for data before they finish executing. 
    
    Removed dead form component in libs.

--- libs/task-design/src/components/form-components/form-field.tsx
+++ libs/task-design/src/components/form-components/form-field.tsx
@@ -1,25 +1,22 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Host, Prop } from '@stencil/core';
 
 @Component({
-  tag: 'form-field',
-  styleUrl: 'form-field.scss',
+    tag: 'form-field',
+    styleUrl: 'form-field.scss',
 })
 export class FormField {
+    @Prop() label: string;
+    @Prop() for: string;
+    @Prop({ reflect: true }) showError: boolean;
+    @Prop() errorMessage: string;
 
-  @Prop() label: string;
-  @Prop() for: string;
-  @Prop() isInvalid: boolean;
-
-  render() {
-    return [
-    <label htmlFor={this.for}>{this.label}</label>,
-    <slot/>,
-
-        // This is what I want to implement but typescript throws an error when I do //    
-
-    // if(this.isInvalid) {
-    //   return <span>{this.label} is required.</span>
-    // }
-    ];
-  }
-} 
+    render() {
+        return (
+            <Host>
+                <label htmlFor={this.for}>{this.label}</label>
+                <slot />
+                <span class='error-message'>{this.errorMessage}</span>
+            </Host>
+        );
+    }
+}
