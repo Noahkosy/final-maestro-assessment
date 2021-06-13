54f4628f33102b6eea6c161038f0be43d71a7cf0
Noah <noah@meetmaestro.com>
2021-06-13 16:58:48 -0400

    Feat(tasks/): Responsive views

--- libs/task-design/src/components/task-detail/task-detail.tsx
+++ libs/task-design/src/components/task-detail/task-detail.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Host, Prop } from '@stencil/core';
 
 @Component({
     tag: 'task-detail',
@@ -17,7 +17,7 @@ export class TaskDetail {
 
     render() {
         return (
-            <div class='detail-card'>
+            <Host class='detail-card'>
                 <p class='section-title'>THUMBNAIL</p>
                 <div class='detail-img mrgn--btm'>
                     <img src='' alt='' />
@@ -34,7 +34,7 @@ export class TaskDetail {
 
                 <p class='section-title'>DUE DATE</p>
                 {/* <div class='detail-calendar mrgn--btm'>
-                    <svg version='1.1' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 32 32'>
+                    <svg class='calendar-color' version='1.1' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 32 32'>
                         <path d='M4 30.667c-1.473 0-2.667-1.194-2.667-2.667v0-21.333c0-1.473 1.194-2.667 2.667-2.667v0l5.999-0.001 0.001-1.332c0-0.736 0.597-1.333 1.333-1.333 0.68 0 1.241 0.509 1.323 1.166l0.010 0.167-0.001 1.332h6.667v-1.332c0-0.736 0.597-1.333 1.333-1.333 0.68 0 1.241 0.509 1.323 1.166l0.010 0.167-0.001 1.332 6.001 0.001c1.395 0 2.54 1.072 2.657 2.437l0.010 0.23v21.333c0 1.473-1.194 2.667-2.667 2.667v0zM9.999 22.666h-6l0.001 5.334 5.999-0.001v-5.333zM19.333 22.666h-6.667v5.333h6.667v-5.333zM27.999 22.666h-6v5.333l6.001 0.001-0.001-5.334zM9.999 14.666h-6v5.333h6v-5.333zM19.333 14.666h-6.667v5.333h6.667v-5.333zM27.999 14.666h-6v5.333h6v-5.333zM9.999 6.666l-5.999 0.001-0.001 5.332h24l0.001-5.332-6.001-0.001 0.001 1.334c0 0.736-0.597 1.333-1.333 1.333-0.68 0-1.241-0.509-1.323-1.166l-0.010-0.167v-1.334h-6.667l0.001 1.334c0 0.736-0.597 1.333-1.333 1.333-0.68 0-1.241-0.509-1.323-1.166l-0.010-0.167-0.001-1.334z'></path>
                     </svg>
                 </div> */}
@@ -47,7 +47,7 @@ export class TaskDetail {
                 <div class='detail-notes'>
                     <p>{this.notes}</p>
                 </div>
-            </div>
+            </Host>
         );
     }
 }
